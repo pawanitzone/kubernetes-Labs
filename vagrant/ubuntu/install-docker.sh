830f0fb367a0bf267302d870df132dcaa315e74a
Pawan Kumar <pawanitzone.kumar@gmail.com>
2022-02-28 11:31:25 +0530

    Update install-docker.sh

--- vagrant/ubuntu/install-docker.sh
+++ vagrant/ubuntu/install-docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive 
+
+##Add condition if OS=Ubuntu then run these commands
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt install docker.io -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+##change cgroupfs to systemd
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{ "exec-opts": ["native.cgroupdriver=systemd"] }
+EOF
